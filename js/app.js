a43aec27beca84cd7ab5e879fb3d3b888ff78dfa
themanh <the.manh@primelabo.com.vn>
2020-04-06 20:54:28 +0700

    update CV, change something

--- js/app.js
+++ js/app.js
@@ -2,18 +2,20 @@
     i18n,
     data:{
         api_key:'AIzaSyCNse-XkpinHLc19NVfVsU5pg6h-drpHJM',
-        client_id:'373033378664-3e1ao1hil9hj9fdbedo49861piqfpekq.apps.googleusercontent.com',
+        client_id:'458833184457-qp8rtn3ql1vsfka4urcb0iruu837jo3s.apps.googleusercontent.com',
         lang:'en',
         copyright:'',
         authorize:{
+            token:'',
             userInfoUrl:"https://github.com/users",
             accessUrl:'https://github.com/login/oauth/authorize?',
-            accessTokenUrl:'https://github.com/login/oauth/access_token',
+            accessTokenUrl:'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
             client_id:'20bb1c4a338408adbca2',
             client_secret:'284a707bb4bd28006f28284d2075a02873e59612',
         },
         ggAuth:_ggAuth,
-        loginUser:''
+        loginUser:'',
+        driveScope:'https://www.googleapis.com/auth/drive'
     },
     mounted() {
         _ggAuth.signin2.render('google-signin-button', {
@@ -21,7 +23,6 @@
         });
     },
     created:function() {
-        this.handleClientLoad();
         i18n.locale = this.lang;
 
         this.copyright = new Date().getFullYear() +'';
@@ -40,7 +41,7 @@
               apiKey: this.api_key,
               clientId: this.client_id,
               discoveryDocs: 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
-              scope: 'https://www.googleapis.com/auth/drive.metadata.readonly'
+              scope: this.driveScope
             }).then(function () {
                 // Listen for sign-in state changes.
                 _ggDriveAuth.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
@@ -94,52 +95,15 @@
         changeLang:function(){
             i18n.locale = (i18n.locale == this.lang )? 'vi' : this.lang;
         },
-        login:function(){
-            $('.g-signin2').click();return;
-            let redirect_uri = window.location.href;
-            let url = this.authorize.accessUrl +"client_id=" + this.authorize.client_id+'&allow_signup=false&redirect_uri='+redirect_uri;
-            window.location.href = url;
-        },
-        decodeFile: function(code) {
-
-        },
-        getAccessToken:function(code) {
-            let _self = this;
-            let redirect_uri = window.location.href;
-            $.ajax({
-                url: this.authorize.accessTokenUrl,
-                type: 'POST',
-                data: {
-                    client_id: _self.authorize.client_id,
-                    client_secret: _self.authorize.client_secret,
-                    code: code,
-                    redirect_uri:redirect_uri
-                },
-            })
-            .done(function(res) {
-                console.log(res );
-                let searchParams = new URLSearchParams(res);
-                let accessToken = searchParams.get('access_token');
-                return _self.getUserInfo(accessToken);
-            }).fail( function(err) {
-                let searchParams = new URLSearchParams(res);
-                let accessToken = searchParams.get('access_token'); 
-                console.log(err);
-            });
-        },
-        getUserInfo:function (token) {
-            $.ajax({
-                url: this.authorize.accessUrl,
-                type: 'GET',
-                headers: {
-                    'Authorization':'token '+token,
-                },
-            })
-            .done(function(res) {
-                console.log(res );
-            }).fail( function(err) {
-                console.log(err);
-            });
+        clickAvt:function(){
+            $('#loginModal').modal('show');
+            if('' != this.loginUser){
+                $('#google-signin-button').hide();
+                $('.avt-img').css('')
+                //background-image: url(images/about.jpg);
+                return this.signOut();
+            }
+            return this.logIn();
         },
         onSignIn(googleUser) {
             this.loginUser = googleUser.getBasicProfile();
@@ -177,10 +141,19 @@
                 onClosed: null,
                 icon_type: 'class',
             });
-			console.log('ID: ' + this.loginUser.getId()); // Do not send to your backend! Use an ID token instead.
-			console.log('Name: ' + this.loginUser.getName());
-			console.log('Image URL: ' + this.loginUser.getImageUrl());
-			console.log('Email: ' + this.loginUser.getEmail()); // This is null if the 'email' scope is not present.
+            this.loginUser.familyName = this.upperCaseFirstLetter(this.loginUser.getFamilyName());
+            this.loginUser.givenName = this.upperCaseFirstLetter(this.loginUser.getGivenName());
+            this.loginUser.fullName = this.loginUser.familyName + ' ' +this.loginUser.givenName;
+            this.loginUser.email = this.loginUser.getEmail();
+            this.loginUser.avartar = this.loginUser.getImageUrl();
+            $('google-signin-button').hide();
+        },
+        upperCaseFirstLetter(str) {
+            str = str.split(" ");
+            for (var i = 0, x = str.length; i < x; i++) {
+                str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+            }
+            return str.join(" ");
         },
         signOut:function() {
             var auth2 = this.ggAuth.auth2.getAuthInstance();
